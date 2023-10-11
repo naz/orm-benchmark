2785f5f7544810bddaf0bdcf2db256c0328e803c
Naz <hi@nazavo.com>
2023-10-11 14:27:59 +0800

    Fixed seeding
    - Made sure there are exactly 1000 contacts with 2 relations each

--- src/database/seeding/initial.seed.ts
+++ src/database/seeding/initial.seed.ts
@@ -1,4 +1,3 @@
-// @/src/db/seeding/seeds/initialSeed.ts
 import { Factory, Seeder } from 'typeorm-seeding';
 import { Connection } from 'typeorm';
 
@@ -11,25 +10,27 @@ export default class InitialDatabaseSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
     const contacts = await factory(Contact)().createMany(1000);
 
-    await factory(Phone)()
-      .map(async (phone) => {
-        phone.contact = contacts[Math.floor(Math.random() * contacts.length)];
-        return phone;
-      })
-      .createMany(2000);
+    for (const contact of contacts) {
+      await factory(Phone)()
+        .map(async (phone) => {
+          phone.contact = contact;
+          return phone;
+        })
+        .createMany(2);
 
-    await factory(Email)()
-      .map(async (email) => {
-        email.contact = contacts[Math.floor(Math.random() * contacts.length)];
-        return email;
-      })
-      .createMany(2000);
+      await factory(Email)()
+        .map(async (email) => {
+          email.contact = contact;
+          return email;
+        })
+        .createMany(2);
 
-    await factory(Address)()
-      .map(async (address) => {
-        address.contact = contacts[Math.floor(Math.random() * contacts.length)];
-        return address;
-      })
-      .createMany(2000);
+      await factory(Address)()
+        .map(async (address) => {
+          address.contact = contact;
+          return address;
+        })
+        .createMany(2);
+    }
   }
 }
